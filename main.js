0d1ed1f91ccfbe448f8b1c53d45790a1df29c99e
Franco <francolens2002@gmail.com>
2024-04-09 18:58:30 -0300

    Sumando carpeta con imagenes de ejemplo, js para formacion y carrusel

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggletext(number){
+    var text = document.getElementById("habilidades" + number);
+    if(text.style.display == "none"){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+
+
+}
+
