d3c6d6d623c691849dbdfea1402f9249d620e746
Franco <francolens2002@gmail.com>
2024-04-22 17:46:00 -0300

    boosteando el proyecto con js para renderizar informacion

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+
+//atributos
+const tech = document.getElementById("tech1");
+const LTech = ['Java', 'HTML5', 'Css', 'Javascript', 'mySql', 'Spring-boot'];
+
+const proyects = document.getElementById("proyectoCarousel");
+
+const proyectInfo = [
+    {
+        titulo: "Producto API",
+        tecnologias: ["Java", 'Spring-Boot', 'Html', 'Css', 'mySQL', 'Tomcat', 'Hibernate'],
+        link: "https://github.com/eelColo/ProductoApi",
+        imgUrl: 'css\assets\examples\producto-api-example.png'
+    },
+    {
+        titulo: "",
+        tecnologias: "",
+        link: "",
+        imgUrl: ""
+    }
+];
+
+
+
+//funciones
+//renderizado de tecnologias
+function renderTech(){
+    for(let element of LTech){
+        tech.innerHTML += `<li class="m-2 d-inline-block">${element}</li>`;
+    }
+}
+
+// Renderizado de proyectos
+function renderProys(){
+    for(let item of proyectInfo){
+        
+    }
+}
+
+
+
 function toggletext(number){
     var text = document.getElementById("habilidades" + number);
     if(text.style.display == "none"){
@@ -9,3 +50,5 @@ function toggletext(number){
 
 }
 
+//eventos
+renderTech();
