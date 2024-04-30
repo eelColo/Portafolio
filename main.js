74b9c941cc77966bed3b63b1f1946bb75ac166be
Franco <francolens2002@gmail.com>
2024-04-29 23:19:47 -0300

    Sumando boton para frenar el wallpaper y modificando css

--- main.js
+++ main.js
@@ -36,6 +36,9 @@ const estudiosInf = [
 
 // Funciones
 
+
+
+
 // Renderizado de tecnologias del HEADER
 function renderTechH(){
     for(let element of listTech){
@@ -97,12 +100,6 @@ function renderStudies(){
 }
 
 
-
-
-
-
-
-
 // Recupera los botones de los estudios, los itera y a cada uno le agrega un addEventListener al click para que tome el id de los resumenes y chequee si tiene la clase d-none y cambiarla por block o none devuelta
 const evButton = () => {
     const estudiosButton = document.querySelectorAll('.enchantingButton');
@@ -123,10 +120,73 @@ const evButton = () => {
     
     });
 }
+
+//Boton para detener el gif
+
+const stopGif = () => {
+    const headerBackground = document.getElementsByName("header");
+    const mainBackground = document.getElementsByName("main");
+
+
+    const stopButton = document.querySelector(".a");
+    const logo = document.querySelector(".a svg");
+    console.log(logo);
+
+    stopButton.addEventListener('click', () => {
+
+        if(logo.getAttribute('id') == "stopButton1"){
+            stopButton.innerHTML = `<svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+            </svg>`
+
+            logo.setAttribute('id', 'playButton1');
+            
+            headerBackground.classList.toggle("pngBack")
+            mainBackground.classList.toggle("pngBack");
+
+
+            
+        } else if (logo.getAttribute('id') == "playButton1") {
+            
+            stopButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-stop-fill" viewBox="0 0 16 16">
+            <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5"/>`
+
+            logo.setAttribute('id', 'stopButton1');
+
+            
+
+
+        }
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //eventos
 document.addEventListener('DOMContentLoaded',() =>{
     renderTechH();
     renderProys();
     renderStudies();
     evButton();
+    stopGif();
 });
